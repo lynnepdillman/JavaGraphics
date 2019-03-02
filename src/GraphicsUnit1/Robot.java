e8091aa65058be3248754aa815d5891ad3c27583
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-03-02 15:50:24 -0800

    Update Robot.java

--- src/GraphicsUnit1/Robot.java
+++ src/GraphicsUnit1/Robot.java
@@ -1,12 +1,10 @@
-package GraphicsUnit1;
-
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Lynne Dillman
+//Date - 3-2-19
+//Class - APCSA
+//Lab  - Robot
 
 import java.awt.Graphics;
 import java.awt.Color;
@@ -14,46 +12,56 @@ import java.awt.Canvas;
 
 class Robot extends Canvas
 {
-   public Robot()    //constructor method - sets up the class
-   {
-      setSize(800,600);
-      setBackground(Color.WHITE);   	
-      setVisible(true);
-   }
+  public Robot()    //constructor method - sets up the class
+  {
+    setSize(800,600);
+    setBackground(Color.WHITE);   	
+    setVisible(true);
+  }
+  
+  public void paint( Graphics window )
+  {
+    window.setColor(Color.BLUE);
+    
+    window.drawString("Robot LAB ", 35, 35 );
+    
+    //call head method
+    head(window);
+    
+    //call other methods
+    upperBody(window);
+    lowerBody(window);
+  }
+  
+  public void head( Graphics window )
+  {
+    window.setColor(Color.BLACK);
+    
+    // head
+    window.drawOval(300, 100, 200, 125);
+    
+    // eyes
+    window.fillOval( 350, 155, 20, 20 );
+    window.fillOval( 430, 155, 20, 20 );
 
-   public void paint( Graphics window )
-   {
-      window.setColor(Color.BLUE);
+    // mouth
+    window.drawArc(355, 150, 90, 20, 180, 180);
+  }
 
-      window.drawString("Robot LAB ", 35, 35 );
+  public void upperBody( Graphics window )
+  {
+    // body
+    window.drawOval(300, 225, 200, 225);
+    
+    // arms
+    window.drawRect(250, 250, 50, 125);
+    window.drawRect(500, 250, 50, 125);
+  }
 
-      //call head method
-      
-      //call other methods
-      
-   }
-
-   public void head( Graphics window )
-   {
-      window.setColor(Color.YELLOW);
-
-      window.fillOval(300, 100, 200, 100);
-
-
-		//add more code here
-				
-   }
-
-   public void upperBody( Graphics window )
-   {
-
-		//add more code here
-   }
-
-   public void lowerBody( Graphics window )
-   {
-
-		//add more code here
-
-   }
+  public void lowerBody( Graphics window )
+  {
+    // legs
+    window.drawRect(335, 450, 50, 75);
+    window.drawRect(415, 450, 50, 75);
+  }
 }
