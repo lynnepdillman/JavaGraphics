44e5eb3d84719506a869c43fde3e61216b1b1f47
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-03-04 09:14:27 -0800

    Update Shape.java

--- src/GraphicsUnit1/Shape.java
+++ src/GraphicsUnit1/Shape.java
@@ -1,109 +1,35 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-
-//Name - Lynne Dillman
-//Date - 3-28-19
-//Class - APCSA
-//Lab  - Shape
+package GraphicsUnit1;
 
 import java.awt.Color;
 import java.awt.Graphics;
 
 public class Shape
 {
-  //instance variables
-  private int xPos;
-  private int yPos;
-  private int width;
-  private int height;
-  private Color color;
+   //instance variables
+   private int xPos;
+   private int yPos;
+   private int width;
+   private int height;
+   private Color color;
 
-  public Shape(int x, int y, int wid, int ht, Color col)
-  {
-    xPos = x;
-    yPos = y;
-    width = wid;
-    height = ht;
-    color = col;
-  }
+   public Shape(int x, int y, int wid, int ht, Color col)
+   {
+	xPos = x; yPos = y; width = wid; height = ht; color = col;
+   }
 
+   public void draw(Graphics window)
+   {
+       
+      window.setColor(color);
+      window.fillRect(xPos, yPos, width, height);
+      window.fillOval(xPos, yPos + height, width, width);
+      window.fillOval(xPos, yPos + height+width, width, width);
+      window.fillRect(xPos, yPos + 2*height, width, width);
+      window.fillOval(xPos, yPos + 2*height+width, width, width);      
+   }
 
-  public void draw(Graphics window)
-  {
-    // color frame
-    window.setColor(color);
-    window.fillRect(xPos - 13, yPos - 13, width + 25, height + 25);  
-    
-    // white background
-    window.setColor(color.WHITE);
-    window.fillRect(xPos, yPos, width, height);
-    
-    // circles
-    int xNew = (width / 2) + xPos - 37;
-    int yNew = (height / 2) + yPos - 37;
-    
-    window.setColor(color);
-    window.drawOval(xNew, yNew, 50, 50);
-    window.drawOval(xNew+25, yNew, 50, 50);
-    window.drawOval(xNew, yNew+25, 50, 50);
-    window.drawOval(xNew+25, yNew+25, 50, 50);
-  }
-
-  
-  //BONUS
-  //add in set and get methods for all instance variables
-
-  public int getxPos() {
-    return xPos;
-  }
-
-
-  public void setxPos(int x) {
-    xPos = x;
-  }
-
-
-  public int getyPos() {
-    return yPos;
-  }
-
-
-  public void setyPos(int y) {
-    yPos = y;
-  }
-
-
-  public int getWidth() {
-    return width;
-  }
-
-
-  public void setWidth(int wid) {
-    width = wid;
-  }
-
-
-  public int getHeight() {
-    return height;
-  }
-
-
-  public void setHeight(int ht) {
-    height = ht;
-  }
-
-
-  public Color getColor() {
-    return color;
-  }
-
-
-  public void setColor(Color col) {
-    color = col;
-  }
-  
-  public String toString()
-  {
-    return xPos+" "+yPos+" "+width+" "+height+" "+color;
-  }
+   public String toString()
+   {
+      return xPos+" "+yPos+" "+width+" "+height+" "+color;
+   }
 }
