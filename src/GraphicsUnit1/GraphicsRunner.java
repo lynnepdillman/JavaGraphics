cccaea18f061cf48d06fe2c668f5614620502b3a
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-03-04 09:13:34 -0800

    Update GraphicsRunner.java

--- src/GraphicsUnit1/GraphicsRunner.java
+++ src/GraphicsUnit1/GraphicsRunner.java
@@ -1,36 +1,29 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-
-//Name - Lynne Dillman
-//Date - 3-2-19
-//Class - APCSA
-//Lab  - GraphicsRunner
+package GraphicsUnit1;
 
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class GraphicsRunner extends JFrame
 {
-  private static final int WIDTH = 800;
-  private static final int HEIGHT = 600;
-  
-  public GraphicsRunner()
-  {
-    super("Graphics Runner");
-      
-    setSize(WIDTH,HEIGHT);
+    private static final int WIDTH  = 800;
+    private static final int HEIGHT = 800;
+
+    public GraphicsRunner() {
+	super("Graphics Runner");
+	setSize(WIDTH,HEIGHT);
+
+	//getContentPane().add(new SmileyFace());	
+	//add other classes to run them 
+	//BigHouse, Robot, or ShapePanel 
+        //getContentPane().add(new BigHouse());
+        //getContentPane().add(new Robot());
+        getContentPane().add(new ShapePanel());
+        
+	setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     
-    getContentPane().add(new SmileyFace());
-    
-    //add other classes to run them 
-    //BigHouse, Robot, or ShapePanel 
-    
-    setVisible(true);
-    
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
-  
-  public static void main(String args[])
-  {
-    GraphicsRunner run = new GraphicsRunner();
-  }
+    public static void main(String args[]) {
+	GraphicsRunner run = new GraphicsRunner();
+    }
 }
